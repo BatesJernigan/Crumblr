5573a466796fd58934dae9192050d6007f890680
BatesJernigan <bates1012@aol.com>
2014-10-23 17:49:03 -0400

    Add Image posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -58,6 +58,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :content)
+      params.require(:post).permit(:title, :content, :kind)
     end
 end
